b0853f47f567e46fb001a759ac23e8438b67954a
Steve Buzonas <steve@fancyguy.com>
2015-03-22 02:59:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# arptables
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'arptables'
+maintainer       'Steve Buzonas'
+maintainer_email 'steve@fancyguy.com'
+license          'mit'
+description      'Installs/Configures arptables'
+long_description 'Installs/Configures arptables'
+version          '0.1.0'
+
